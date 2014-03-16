51f32d5b9ddf440f923eda98be5d77cf39b970b4
Alan <altano@gmail.com>
2014-03-16 15:33:32 -0400

    Fixed helpers.

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ module.exports = function(source) {
 	var foundUnclearStuff = {};
 	var knownHelpers = {};
 
+	var debug = false;
+
 	var hb = handlebars.create();
 	var JavaScriptCompiler = hb.JavaScriptCompiler;
 	function MyJavaScriptCompiler() {
@@ -31,26 +33,31 @@ module.exports = function(source) {
 	MyJavaScriptCompiler.prototype = Object.create(JavaScriptCompiler.prototype);
 	MyJavaScriptCompiler.prototype.compiler = MyJavaScriptCompiler;
 	MyJavaScriptCompiler.prototype.nameLookup = function(parent, name, type) {
+		if (debug) {
+			console.log("nameLookup %s %s %s", parent, name, type);
+		}
 		if (type === "partial") {
 			if (foundPartials["$" + name]) {
 				return "require(" + JSON.stringify(foundPartials["$" + name]) + ")";
 			}
 			foundPartials["$" + name] = null;
-			return JavaScriptCompiler.prototype.nameLookup.call(this, parent, name, type);
+			return JavaScriptCompiler.prototype.nameLookup.apply(this, arguments);
 		}
 		else if (type === "helper") {
+			debugger;
 			if (foundHelpers["$" + name]) {
 				return "require(" + JSON.stringify(foundHelpers["$" + name]) + ")";
 			}
-			return JavaScriptCompiler.prototype.nameLookup.call(this, parent, name, type);
+			foundHelpers["$" + name] = null;
+			return JavaScriptCompiler.prototype.nameLookup.apply(this, arguments);
 		}
 		else if (type === "context") {
 			// This could be a helper too, save it to check it later
 			if (!foundUnclearStuff["$" + name]) foundUnclearStuff["$" + name] = false;
-			return JavaScriptCompiler.prototype.nameLookup.call(this, parent, name, type);
+			return JavaScriptCompiler.prototype.nameLookup.apply(this, arguments);
 		}
 		else {
-			return JavaScriptCompiler.prototype.nameLookup.call(this, parent, name, type);
+			return JavaScriptCompiler.prototype.nameLookup.apply(this, arguments);
 		}
 	};
 	hb.JavaScriptCompiler = MyJavaScriptCompiler;
@@ -58,18 +65,24 @@ module.exports = function(source) {
 	// This is an async loader
 	var loaderAsyncCallback = this.async();
 
+	var firstCompile = true;
+	var compilationPass = 0;
+
 	(function compile() {
+		if (debug) {
+			console.log("\nCompilation pass %d", ++compilationPass);
+		}
+
 		// Need another compiler pass?
 		var needRecompile = false;
 
 		// Precompile template
 		var template = hb.precompile(source, {
-			knownHelpersOnly: true,
+			knownHelpersOnly: firstCompile ? false : true,
 			knownHelpers: knownHelpers
 		});
 
-		// Check for each found unclear item if it is a helper
-		async.forEach(Object.keys(foundUnclearStuff), function(stuff, unclearStuffCallback) {
+		var resolveUnclearStuffIterator = function(stuff, unclearStuffCallback) {
 			if (foundUnclearStuff[stuff]) return unclearStuffCallback();
 			var request = referenceToRequest(stuff.substr(1));
 			loaderApi.resolve(loaderApi.context, request, function(err, result) {
@@ -81,45 +94,110 @@ module.exports = function(source) {
 				foundUnclearStuff[stuff] = true;
 				unclearStuffCallback();
 			});
-		}, function() {
+		};
+
+		var resolvePartialsIterator = function(partial, partialCallback) {
+			if (foundPartials[partial]) return partialCallback();
+			var request = referenceToRequest(partial.substr(1));
+
+			// Try every extension for partials
+			var i = 0;
+			(function tryExtension() {
+				var errorMsg = util.format("Partial '%s' not found", partial.substr(1));
+				if (i > extensions.length) return partialCallback(new Error(errorMsg));
+				var extension = extensions[i++];
+
+				if (debug) {
+					var path = require("path");
+					var partialTrace = path.normalize(loaderApi.context + "\\" + request + extension);
+					loaderApi.emitWarning("Attempting to resolve partial %s", partialTrace);
+				}
+
+				loaderApi.resolve(loaderApi.context, request + extension, function(err, result) {
+					if (!err && result) {
+						foundPartials[partial] = result;
+						needRecompile = true;
+						return partialCallback();
+					}
+					tryExtension();
+				});
+			}());
+		};
+
+		var resolveHelpersIterator = function(helper, helperCallback) {
+			debugger;
+
+			if (foundHelpers[helper]) return helperCallback();
+			var request = referenceToRequest(helper.substr(1));
+
+			if (debug) {
+				var path = require("path");
+				var helperTrace = path.normalize(loaderApi.context + "\\" + request);
+				loaderApi.emitWarning("Attempting to resolve helper %s", helperTrace);
+			}
+
+			loaderApi.resolve(loaderApi.context, request, function(err, result) {
+				if (!err && result) {
+					knownHelpers[helper.substr(1)] = true;
+					foundHelpers[helper] = result;
+					needRecompile = true;
+					return helperCallback();
+				}
+				var errorMsg = util.format("Helper '%s' not found", helper.substr(1));
+				helperCallback(new Error(errorMsg));
+			});
+		};
+
+		var doneResolving = function(err) {
+			if (err) return loaderAsyncCallback(err);
+
+			// Do another compiler pass if not everything was resolved
+			if (needRecompile) {
+				firstCompile = false;
+				return compile();
+			}
+
+			// export as module
+			loaderAsyncCallback(null, 'module.exports = require(' + JSON.stringify(runtimePath) + ').default.template(' + template + ');');
+		};
+
+		var resolvePartials = function(err) {
+			if (err) throw err;
+
+			if (debug) {
+				console.log("Attempting to resolve partials:");
+				console.log(foundPartials);
+			}
 
 			// Resolve path for each partial
-			async.forEach(Object.keys(foundPartials), function(partial, partialCallback) {
-				if (foundPartials[partial]) return partialCallback();
-				var request = referenceToRequest(partial.substr(1));
+			async.forEach(Object.keys(foundPartials), resolvePartialsIterator, doneResolving);
+		};
 
-				// Try every extension for partials
-				var i = 0;
-				(function tryExtension() {
-					var errorMsg = util.format("Partial '%s' not found", partial.substr(1));
-					if (i > extensions.length) return partialCallback(new Error(errorMsg));
-					var extension = extensions[i++];
+		var resolveUnclearStuff = function(err) {
+			if (err) throw err;
 
-					if (this.debug) {
-						var path = require("path");
-						var partialTrace = path.normalize(loaderApi.context + "\\" + request + extension);
-						this.emitWarning("Attempting to resolve %s", partialTrace);
-					}
+			if (debug) {
+				console.log("Attempting to resolve unclearStuff:");
+				console.log(foundUnclearStuff);
+			}
 
-					loaderApi.resolve(loaderApi.context, request + extension, function(err, result) {
-						if (!err && result) {
-							foundPartials[partial] = result;
-							needRecompile = true;
-							return partialCallback();
-						}
-						tryExtension();
-					});
-				}());
-			}, function(err) {
-				if (err) return loaderAsyncCallback(err);
+			// Check for each found unclear item if it is a helper
+			async.forEach(Object.keys(foundUnclearStuff), resolveUnclearStuffIterator, resolvePartials);
+		};
 
-				// Do another compiler pass if not everything was resolved
-				if (needRecompile) return compile();
+		var resolveHelpers = function(err) {
+			if (err) throw err;
 
-				// export as module
-				loaderAsyncCallback(null, 'module.exports = require(' + JSON.stringify(runtimePath) + ').default.template(' + template + ');');
-			});
-		});
+			if (debug) {
+				console.log("Attempting to resolve helpers:");
+				console.log(foundHelpers);
+			}
+
+			// Resolve path for each helper
+			async.forEach(Object.keys(foundHelpers), resolveHelpersIterator, resolveUnclearStuff);
+		};
+
+		resolveHelpers();
 	}());
 };
 
